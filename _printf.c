be7752372b872b0ab41e66db11d59f57cc9bf570
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-08-23 20:16:51 +0300

    debug

--- _printf.c
+++ _printf.c
@@ -17,8 +17,12 @@ int _printf(const char *format, ...)
 	va_start(ap, format);
 	while (format[i] != '\0')
 	{
+		if (format == NULL || (format[i] == '%' && format[i + 1] == '\0'))
+		return (-1);
 		if (format[i] == '%')
 		{
+			if (format[i + 1] == ' ')
+			return (-1);
 			if (format[i + 1] != '\0')
 			{
 				int valid_spec = 0;
