1e69ada96d5980b5b0595fa90bf06c06582ef6ad
Ahmed Mohamed Marei <68832219+AHMEDMOHAMED096@users.noreply.github.com>
2023-08-26 14:20:15 +0300

    debug

--- _printf.c
+++ _printf.c
@@ -17,7 +17,7 @@ int _printf(const char *format, ...)
 	va_start(ap, format);
 	while (format[i] != '\0')
 	{
-		if (format == NULL || (format[i] == '%' && format[i + 1] == '\0'))
+		if (!format)
 		return (-1);
 		if (format[i] == '%')
 		{
